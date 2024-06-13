860ac13e4f380bee8bab94ce12e6472c97f07b22
Kumar Zhekshenbek uulu <zhekshenbekukumar@gmail.com>
2024-06-13 12:39:53 +0600

    git action number 2

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,25 +1,30 @@
-import HTMLWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import { BuildOptions } from './types/config';
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import { BuildOptions } from "./types/config";
 
 export const buildPlugins = ({
     paths,
     isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-    new HTMLWebpackPlugin({
-        template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-    }),
-    isDev ? new ReactRefreshWebpackPlugin() : null,
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+    ];
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+    return plugins;
+};
